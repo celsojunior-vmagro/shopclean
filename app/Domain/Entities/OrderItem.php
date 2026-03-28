78f80fa53fddb82f2fb05307e45d9ddf7e7837f0
Celso Junior <celso@vmagrodigital.com.br>
2026-03-28 18:40:25 -0300

    feat: add entidade do item do pedido

---
+++ app/Domain/Entities/OrderItem.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Domain\Entities;
+
+class OrderItem
+{
+    private function __construct(
+        private readonly ?string $id,
+        private readonly string  $productId,
+        private readonly string  $productName,
+        private readonly float   $unitPrice,
+        private readonly int     $quantity,
+    )
+    {
+        $this->validate();
+    }
+
+    private function validate(): void
+    {
+        if ($this->quantity <= 0) {
+            throw new \InvalidArgumentException('A quantidade deve ser maior que zero');
+        }
+
+        if ($this->unitPrice <= 0) {
+            throw new \InvalidArgumentException('O Preço deve ser maior que zero');
+        }
+    }
+
+    public static function create
+    (
+        string $productId,
+        string $productName,
+        float  $unitPrice,
+        int    $quantity,
+    ): self
+    {
+        return new self(null, $productId, $productName, $unitPrice, $quantity);
+    }
+
+    public static function reconstitute(
+        string $id,
+        string $productId,
+        string $productName,
+        float  $unitPrice,
+        int    $quantity
+    ): self
+    {
+        return new self($id, $productId, $productName, $unitPrice, $quantity);
+    }
+
+    public function getSubtotal(): float
+    {
+        return $this->unitPrice * $this->quantity;
+    }
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    public function getProductId(): ?string
+    {
+        return $this->productId;
+    }
+
+    public function getProductName(): string
+    {
+        return $this->productName;
+    }
+
+    public function getUnitPrice(): float
+    {
+        return $this->unitPrice;
+    }
+
+    public function getQuantity(): int
+    {
+        return $this->quantity;
+    }
+}
